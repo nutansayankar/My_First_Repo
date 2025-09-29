010ce4a59415f265142224ad5a88606ae6266877
Nutan Sayankar <nutansaynkar0101@gmail.com>
2025-09-29 16:20:24 +0530

    Add files via upload

---
+++ sorting_module/sort.py
@@ -0,0 +1,48 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
+
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left=merge_sort(arr[:mid])
+    right=merge_sort(arr[mid:])
+    return merge(left,right)
+def merge(left,right):
+    result=[]
+    i=j=0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
